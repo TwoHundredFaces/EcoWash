1f0c7c2e0368dab75faa2dbcb7f0809e8b4ece33
TwoHundredFaces <emersonsoares141@gmail.com>
2023-09-12 22:26:17 -0300

    SIDEBAR FUNCIONAL

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 document.getElementById("imagem").innerHTML = "<img src='imgs/logo.png'>";
 document.getElementById("perfil").innerHTML = "<img src='imgs/Emerson.jpg'>";
+
+let sidebar = document.querySelector('.sidebar');
+let sidebarBtn = document.querySelector('.bx-menu');
+
+
+sidebarBtn.addEventListener('click', () =>{
+    sidebar.classList.toggle('close')
+} );
