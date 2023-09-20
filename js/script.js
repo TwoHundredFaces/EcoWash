7978e9906d42e594154cbad3e738c71571b23e1a
TwoHundredFaces <emersonsoares141@gmail.com>
2023-09-20 10:25:59 -0300

    Atualização

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 document.getElementById("imagem").innerHTML = "<img src='imgs/logo.png'>";
 document.getElementById("perfil").innerHTML = "<img src='imgs/Emerson.jpg'>";
+
+// Criação das variáveis da sidebar e do botão abrir/fechar
+
+let sidebar = document.querySelector('.sidebar');
+let sidebarBtn = document.querySelector('.bx-menu');
+
+// Aplicação da ação de abrir e fechar da sidebar
+
+sidebarBtn.addEventListener('click', () =>{
+    sidebar.classList.toggle('close')
+} );
